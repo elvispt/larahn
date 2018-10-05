6fdcdb71ac09446fa34c9817fadb6adc0431a916
Elvis Pestana <elvispt@gmail.com>
2018-10-05 10:59:39 +0100

    Add simple monochromatic design to list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
-  <ul>
-    @foreach ($items as $item)
-      <li>
-        <p>
+  @foreach ($items as $item)
+    <div class="row mb-2 ax">
+      <div class="col-10">
+        <div class="ax--left">
           <a href="{{ route('item', ['id' => $item->id]) }}">{{ $item->title }}</a>
-          |
-          @isset($item->url)
-            <a href="{{ $item->url }}" rel="nofollow" target="__{{ $item->id }}">[>]</a></p>
-          @endisset
-        <p><small>S: {{ $item->score }} | C: {{ $item->descendants }} | {{ \Carbon\Carbon::createFromTimestamp($item->time)->format('Y-m-d H:i:s') }}</small></p>
-      </li>
-    @endforeach
-  </ul>
+          <div>
+            <small>S: {{ $item->score }} | C: {{ $item->descendants }} | {{ \Carbon\Carbon::createFromTimestamp($item->time)->format('Y-m-d H:i:s') }}</small>
+          </div>
+        </div>
+      </div>
+      <div class="col-2">
+        @isset($item->url)
+          <a href="{{ $item->url }}" rel="nofollow" target="__{{ $item->id }}" class="ax--icon"></a>
+        @endisset
+      </div>
+    </div>
+    <hr>
+  @endforeach
 @endsection
